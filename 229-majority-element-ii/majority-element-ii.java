8afcd12a63eb7967ba1e90819c3d1fdeb06da071
AnjaliSingh0999 <142236757+AnjaliSingh0999@users.noreply.github.com>
2025-04-26 12:10:08 +0530

    Time: 4 ms (56.20%) | Memory: 48.4 MB (30.28%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.java
+++ 229-majority-element-ii/majority-element-ii.java
@@ -1,17 +1,26 @@
+import java.util.*;
+
 class Solution {
     public List<Integer> majorityElement(int[] nums) {
-        Map<Integer, Integer> freq = new HashMap<>();
+        Arrays.sort(nums);  
         List<Integer> result = new ArrayList<>();
         int n = nums.length;
+        int count = 1;
 
-        for (int num : nums) {
-            freq.put(num, freq.getOrDefault(num, 0) + 1);
+        for (int i = 1; i < n; i++) {
+            if (nums[i] == nums[i - 1]) {
+                count++;
+            } else {
+                if (count > n / 3) {
+                    result.add(nums[i - 1]);
+                }
+                count = 1;  
+            }
         }
 
-        for (Map.Entry<Integer, Integer> entry : freq.entrySet()) {
-            if (entry.getValue() > n / 3) {
-                result.add(entry.getKey());
-            }
+      
+        if (count > n / 3) {
+            result.add(nums[n - 1]);
         }
 
         return result;
