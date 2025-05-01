409919e034a160f3686627f8300435615fbba0a5
AnjaliSingh0999 <142236757+AnjaliSingh0999@users.noreply.github.com>
2025-05-01 14:19:22 +0530

    Time: 0 ms (100.00%) | Memory: 43.1 MB (19.75%) - LeetSync

---
+++ 1552-build-an-array-with-stack-operations/build-an-array-with-stack-operations.java
@@ -0,0 +1,18 @@
+public class Solution{
+    public List<String> buildArray(int[] target, int n) {
+        List<String> result = new ArrayList<>();
+        int targetIndex = 0;
+
+        for (int num = 1; num <= n && targetIndex < target.length; num++) {
+            result.add("Push");
+
+            if (target[targetIndex] == num) {
+                targetIndex++;
+            } else {
+                result.add("Pop");
+            }
+        }
+
+        return result;
+    }
+}
